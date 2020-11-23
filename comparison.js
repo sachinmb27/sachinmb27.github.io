510769a849a899c34c40b69434af6bfbe0d4d260
Sachin Manimekalai Balakrishnan <smanim2@uic.edu>
2020-11-22 22:52:08 -0600

    Revert "New template with sunburst comparison"
    This reverts commit 0724f264d55de2c1266011b803b0e43b0b5929f9.

--- comparison.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.combine').on('change', function(){
-        var combinedStates = $('#filter').val() + $('#state1').val() + $('#state2').val();
-        var radioValue = $("input[name='gender']:checked").val() + '.json';
-        $('#combinedStates').val(combinedStates+radioValue);
-        console.log(combinedStates+radioValue);
-    });
-})
